f4f9ddd3d46192b045813f0c36cde6a3c07623b4
GustavoMSevero <gustavo.msevero@gmail.com>
2020-09-02 19:00:01 -0300

    Desafio typeorm upload concluído

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, TransactionRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRespository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRespository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+
+    await transactionsRespository.remove(transaction);
   }
 }
 
